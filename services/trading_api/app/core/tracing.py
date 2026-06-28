c2725b72110fe8c1134b266a72544e8b8428af31
saranyaravi89 <saranyasakthivel89@gmail.com>
2026-06-28 13:09:54 +0800

    Jaeger tracing

---
+++ services/trading_api/app/core/tracing.py
@@ -0,0 +1,18 @@
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+def setup_tracing():
+    provider = TracerProvider(
+        resource=Resource.create({"service.name": "trading-api"})
+    )
+
+    exporter = OTLPSpanExporter(
+        endpoint="http://jaeger:4317",
+        insecure=True
+    )
+
+    provider.add_span_processor(BatchSpanProcessor(exporter))
+    trace.set_tracer_provider(provider)
--- services/trading_api/app/main.py
+++ services/trading_api/app/main.py
@@ -3,10 +3,15 @@ from app.api.health import router as health_router
 from app.api.orders import router as orders_router
 from app.observability.metrics import router as metrics_router, REQUEST_COUNT
 from app.core.logging import get_logger
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from app.core.tracing import setup_tracing
 
 logger = get_logger()
 
 app = FastAPI()
+
+setup_tracing()
+FastAPIInstrumentor.instrument_app(app)
 
 logger.info("Trading API started")
 
